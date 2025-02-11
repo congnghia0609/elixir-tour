c290419226d4b84f95d09468cd08fae44e5f3433
congnghia0609 <congnghia0609@gmail.com>
2025-02-12 04:47:56 +0700

    add new KV.RegistryTest

---
+++ kv/test/kv/registry_test.exs
@@ -0,0 +1,26 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    ## Dùng start_supervised! để ExUnit đảm bảo rằng tiến trình
+    ## registry sẽ bị tắt shutdown trước khi test tiếp theo bắt đầu.
+    ## Đảm bảo rằng trạng thái của test hiện tại không ảnh hưởng
+    ## đến test tiếp theo trong trường hợp chúng phụ thuộc vào các
+    ## tài nguyên được chia sẻ.
+    registry = start_supervised!(KV.Registry)
+    %{registry: registry}
+  end
+
+  test "spawns buckets", %{registry: registry} do
+    name = "shopping"
+    assert KV.Registry.lookup(registry, name) == :error
+
+    KV.Registry.create(registry, name)
+    assert {:ok, bucket} = KV.Registry.lookup(registry, name)
+
+    key = "milk"
+    value = 1
+    KV.Bucket.put(bucket, key, value)
+    assert KV.Bucket.get(bucket, key) == value
+  end
+end
